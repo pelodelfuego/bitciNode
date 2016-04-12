3c4c522629e92b30e292308283bb94c7b11b9504
pelodelfuego <clement.crepy@gmail.com>
2016-04-13 00:05:43 +0200

    add reading json file + split files

--- bitciNode.js
+++ bitciNode.js
@@ -2,20 +2,26 @@
 var express    = require('express')
 var bodyParser = require('body-parser')
 
+var own        = require('./openWebNetTranslator')
+
 var app = express()
 app.use(bodyParser.json());
 
+var config = {
+    onOff: require('./config/onOff.json')
+};
+
 app.route('/')
     .get(function(req, res) {
-        console.log('Get');
-        res.send('Get');
+        console.log('Get')
+        res.send('Get')
     })
     .post(function(req, res) {
-        console.log('Post');
-        console.log(req.body);
-        res.send('Post');
+        console.log('Post')
+        console.log(req.body)
+        res.send('Post')
     });
 
 app.listen(3000, function () {
-  console.log('Start');
+  console.log('Start')
 });
---
+++ openWebNetTranslator.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    ack: '#1',
+    nack: '#0',
+    foo: function () {
+        // whatever
+    },
+    bar: function () {
+        // whatever
+    }
+};
